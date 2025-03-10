db5dbc8d145870ba3e67d681a1b7d287c84da0dc
Tuan Nguyen <tuancoltech@gmail.com>
2025-03-10 13:20:04 +0700

    Remove unused code/file. Add root build.gradle

--- app/src/main/java/ai/elimu/filamu/BaseApplication.kt
+++ app/src/main/java/ai/elimu/filamu/BaseApplication.kt
@@ -1,38 +1,14 @@
 package ai.elimu.filamu
 
 import android.app.Application
-import android.speech.tts.TextToSpeech
-import android.speech.tts.TextToSpeech.OnInitListener
-import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
+import timber.log.Timber
 
 @HiltAndroidApp
 class BaseApplication : Application() {
-    var executor: Executor = Executors.newSingleThreadExecutor()
-
-    var tTS: TextToSpeech? = null
-        private set
 
     override fun onCreate() {
-        Log.i(javaClass.name, "onCreate")
         super.onCreate()
-
-        // Initialize the Text-to-Speech (TTS) engine
-        tTS = TextToSpeech(applicationContext, object : OnInitListener {
-            override fun onInit(status: Int) {
-                Log.i(javaClass.name, "onInit")
-
-                // Fetch the chosen language from the Appstore
-                // TODO
-//                tts.setLanguage(new Locale("hin"));
-                tTS!!.setSpeechRate(SPEECH_RATE)
-            }
-        })
-    }
-
-    companion object {
-        const val SPEECH_RATE: Float = 0.5f
+        Timber.tag(javaClass.name).i("onCreate")
     }
 }
