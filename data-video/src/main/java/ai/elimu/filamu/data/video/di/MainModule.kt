0283106a39210d9ccc63f7b9c6fa256352dd9db8
Tuan Nguyen <tuancoltech@gmail.com>
2025-03-11 15:23:45 +0700

    Correct component annotation for MainModule

--- data-video/src/main/java/ai/elimu/filamu/data/video/di/MainModule.kt
+++ data-video/src/main/java/ai/elimu/filamu/data/video/di/MainModule.kt
@@ -5,12 +5,12 @@ import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 object MainModule {
     @Singleton
     @Provides
